e3fa62e7e80e338546ff7e85e7f87cf82655aaf2
Caleb Hicks <calebhicks@gmail.com>
2014-06-17 16:05:04 -0600

    commented out ttworkperiod

--- TTWorkPeriod.h
+++ TTWorkPeriod.h
@@ -1,22 +1,22 @@
+////
+////  TTWorkPeriod.h
+////  Wired In
+////
+////  Created by Caleb Hicks on 6/7/14.
+////  Copyright (c) 2014 DevMountain. All rights reserved.
+////
 //
-//  TTWorkPeriod.h
-//  Wired In
+//#import <Foundation/Foundation.h>
 //
-//  Created by Caleb Hicks on 6/7/14.
-//  Copyright (c) 2014 DevMountain. All rights reserved.
+//@interface TTWorkPeriod : NSObject
 //
-
-#import <Foundation/Foundation.h>
-
-@interface TTWorkPeriod : NSObject
-
-@property (strong, nonatomic) NSString *periodTitle;
-@property (strong, nonatomic) NSDate *startTime;
-@property (strong, nonatomic) NSDate *finishTime;
-@property (strong, nonatomic) NSString *description;
-@property (assign, nonatomic) NSTimeInterval duration;
-
-- (NSDictionary *)workPeriodDictionary;
-- (id)initWithDictionary:(NSDictionary *)dictionary;
-
-@end
+//@property (strong, nonatomic) NSString *periodTitle;
+//@property (strong, nonatomic) NSDate *startTime;
+//@property (strong, nonatomic) NSDate *finishTime;
+//@property (strong, nonatomic) NSString *description;
+//@property (assign, nonatomic) NSTimeInterval duration;
+//
+//- (NSDictionary *)workPeriodDictionary;
+//- (id)initWithDictionary:(NSDictionary *)dictionary;
+//
+//@end
--- TTWorkPeriod.m
+++ TTWorkPeriod.m
@@ -1,66 +1,66 @@
+////
+////  TTWorkPeriod.m
+////  Wired In
+////
+////  Created by Caleb Hicks on 6/7/14.
+////  Copyright (c) 2014 DevMountain. All rights reserved.
+////
 //
-//  TTWorkPeriod.m
-//  Wired In
+//#import "TTWorkPeriod.h"
 //
-//  Created by Caleb Hicks on 6/7/14.
-//  Copyright (c) 2014 DevMountain. All rights reserved.
+//static NSString * const titleKey = @"title";
+//static NSString * const startKey = @"start";
+//static NSString * const finishKey = @"finish";
+//static NSString * const descriptionKey = @"description";
+//static NSString * const durationKey = @"duration";
 //
-
-#import "TTWorkPeriod.h"
-
-static NSString * const titleKey = @"title";
-static NSString * const startKey = @"start";
-static NSString * const finishKey = @"finish";
-static NSString * const descriptionKey = @"description";
-static NSString * const durationKey = @"duration";
-
-
-@implementation TTWorkPeriod
-
-- (NSDictionary *)workPeriodDictionary{
-    
-    NSMutableDictionary *entryDictionary = [[NSMutableDictionary alloc]init];
-    
-    if (self.periodTitle) {
-        [entryDictionary setValue:self.periodTitle forKeyPath:titleKey];
-    }
-    
-    if (self.startTime) {
-        [entryDictionary setValue:self.startTime forKeyPath:startKey];
-    }
-    
-    if (self.finishTime){
-        [entryDictionary setValue:self.finishTime forKey:finishKey];
-    }
-    
-    if (self.description){
-        [entryDictionary setValue:self.description forKeyPath:descriptionKey];
-    }
-    
-    if (self.duration){
-        NSInteger durationInt = self.duration;
-        
-        [entryDictionary setValue:[NSNumber numberWithInteger:durationInt] forKey:durationKey];
-    }
-    
-    return entryDictionary;
-}
-
-
-- (id)initWithDictionary:(NSDictionary *)dictionary{
-    self = [super init];
-    if (self) {
-        self.periodTitle = dictionary[titleKey];
-        self.startTime = dictionary[startKey];
-        self.finishTime = dictionary[finishKey];
-        self.description = dictionary[descriptionKey];
-        
-        NSInteger duration = [dictionary[durationKey] integerValue];
-        
-        self.duration = duration;
-
-    }
-    return self;
-}
-
-@end
+//
+//@implementation TTWorkPeriod
+//
+//- (NSDictionary *)workPeriodDictionary{
+//    
+//    NSMutableDictionary *entryDictionary = [[NSMutableDictionary alloc]init];
+//    
+//    if (self.periodTitle) {
+//        [entryDictionary setValue:self.periodTitle forKeyPath:titleKey];
+//    }
+//    
+//    if (self.startTime) {
+//        [entryDictionary setValue:self.startTime forKeyPath:startKey];
+//    }
+//    
+//    if (self.finishTime){
+//        [entryDictionary setValue:self.finishTime forKey:finishKey];
+//    }
+//    
+//    if (self.description){
+//        [entryDictionary setValue:self.description forKeyPath:descriptionKey];
+//    }
+//    
+//    if (self.duration){
+//        NSInteger durationInt = self.duration;
+//        
+//        [entryDictionary setValue:[NSNumber numberWithInteger:durationInt] forKey:durationKey];
+//    }
+//    
+//    return entryDictionary;
+//}
+//
+//
+//- (id)initWithDictionary:(NSDictionary *)dictionary{
+//    self = [super init];
+//    if (self) {
+//        self.periodTitle = dictionary[titleKey];
+//        self.startTime = dictionary[startKey];
+//        self.finishTime = dictionary[finishKey];
+//        self.description = dictionary[descriptionKey];
+//        
+//        NSInteger duration = [dictionary[durationKey] integerValue];
+//        
+//        self.duration = duration;
+//
+//    }
+//    return self;
+//}
+//
+//@end
